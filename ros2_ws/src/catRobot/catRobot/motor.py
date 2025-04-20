06551eaed3ee1025c0ed3e935de7033d4911583b
Lyn Grimes <fmgrimes@ncsu.edu>
2025-04-20 13:19:18 -0400

    updated comments of motor py code

--- ros2_ws/src/catRobot/catRobot/motor.py
+++ ros2_ws/src/catRobot/catRobot/motor.py
@@ -8,6 +8,9 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 
+# The idea behind this code is, recieve the bool message frm play_with_cat_topic. 
+# If this is a True message, spin the motors forward for 5 seconds, and spin the motor reverse for 5 seconds
+
 # Configuration
 FORWARD = 18 # Pin. RYAN CHANGE THIS
 REVERSE = 19 # Pin. RYAN CHANGE THIS
@@ -35,19 +38,18 @@ try:
         self.playing_with_cat = msg.data
         self.get_logger().info(f'Received cat status: {self.playing_with_cat}')
         while self.playing_with_cat:
-          # Turn the prismatic joint to 10% speed for 5 seconds
+          # Turn the motor forward at 10% speed for 5 seconds
           lgpio.tx_pwm(h, FORWARD, FREQ, 10)
           time.sleep(5)
-          # Turn the continuos joing to 10% speed for 10 seconds
+          # Turn the motor reverse at 10% speed for 10 seconds
           lgpio.tx_pwm(h, REVERSE, FREQ, 10)
           time.sleep(5)
           
     
 except KeyboardInterrupt:
-    # Turn the continous joint off
-    lgpio.tx_pwm(h, CONTINUOS, FREQ, 0)
-     # Turn the prismatic joint off
-    lgpio.tx_pwm(h, PRISMATIC, FREQ, 0)
+    # Stop sending power to both pins
+    lgpio.tx_pwm(h, FORWARD, FREQ, 0)
+    lgpio.tx_pwm(h, REVERSE, FREQ, 0)
     lgpio.gpiochip_close(h)
 
 
